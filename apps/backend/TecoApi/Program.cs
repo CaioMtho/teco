4addb4c378729291d9e6da77db36ed62668b73de
CaioMtho <caiomathol@gmail.com>
2025-10-10 23:28:06 -0300

    feat: autenticação e autorização

--- apps/backend/TecoApi/Program.cs
+++ apps/backend/TecoApi/Program.cs
@@ -2,11 +2,18 @@
 using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using TecoApi.Data;
-
+using TecoApi.Helpers;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load("../../../.env");
+
+var jwtKey = builder.Configuration["Jwt:Key"] ?? Environment.GetEnvironmentVariable("JWT_KEY");
+var jwtIssuer = builder.Configuration["Jwt:Issuer"] ?? Environment.GetEnvironmentVariable("JWT_ISSUER");
+var jwtAudience = builder.Configuration["Jwt:Audience"] ?? Environment.GetEnvironmentVariable("JWT_AUDIENCE");
 
 var connectionString = Environment.GetEnvironmentVariable("DEFAULT_CONNECTION");
 builder.Services.AddHealthChecks();
@@ -25,6 +32,31 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtIssuer,
+        ValidAudience = jwtAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+        ClockSkew = TimeSpan.FromMinutes(2)
+    };
+});
+
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
 app.MapHealthChecks("/health");
 app.UseCors("AllowAll");
@@ -38,10 +70,12 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () =>
 {
-    return Results.Ok("Olá Mundo!");
+    return Results.Ok("API rodando...");
 });
 
 app.UseHttpsRedirection();
