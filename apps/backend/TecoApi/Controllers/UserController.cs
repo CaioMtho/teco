8186423ee74784d1f5419c2307f5a01bc61618fd
CaioMtho <caiomathol@gmail.com>
2025-10-11 00:12:45 -0300

    feat: user controller

---
+++ apps/backend/TecoApi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using TecoApi.DTOs.User;
+using TecoApi.Services;
+
+namespace TecoApi.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class UserController(IUserService userService) : ControllerBase
+{
+    private readonly IUserService _userService = userService;
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync(long id)
+    {
+        return Ok(await _userService.GetByIdAsync(id));
+    }
+
+    [HttpGet("by-email")]
+    public async Task<IActionResult> GetByEmailAsync([FromQuery]string email)
+    {
+        return Ok(await _userService.GetByEmailAsync(email));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromBody] CreateUserDto createUserDto)
+    {
+        var user = await _userService.CreateAsync(createUserDto);
+        return CreatedAtAction(nameof(GetByIdAsync), new { id = user.Id }, user);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateAsync(long id, [FromBody] UpdateUserDto updateUserDto)
+    {
+        var user = await _userService.UpdateAsync(id, updateUserDto);
+        return Ok(user);
+    }
+}
