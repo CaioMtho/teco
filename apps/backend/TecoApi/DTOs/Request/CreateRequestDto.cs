b06e3ce468bfc516393b0bebc9821b3f68f1cbec
CaioMtho <caiomathol@gmail.com>
2025-10-13 17:32:45 -0300

    feat: dtos de request

---
+++ apps/backend/TecoApi/DTOs/Request/CreateRequestDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TecoApi.DTOs.Request;
+
+public class CreateRequestDto
+{
+    [Required]
+    [StringLength(100, MinimumLength = 5)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [StringLength(1000, MinimumLength = 10)]
+    public string Description { get; set; } = null!;
+
+    public List<string>? Photos { get; set; }
+    [Required]
+    public long RequesterId { get; set; }
+
+    public long? ServiceAddressId { get; set; }
+}
