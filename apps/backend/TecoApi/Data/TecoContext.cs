7ad61ea1ee95f0ff8f841b745aaf573833086d0a
CaioMtho <caiomathol@gmail.com>
2025-10-13 17:52:35 -0300

    feat: nova migration

--- apps/backend/TecoApi/Data/TecoContext.cs
+++ apps/backend/TecoApi/Data/TecoContext.cs
@@ -13,6 +13,7 @@ public class TecoContext(DbContextOptions<TecoContext> options) : DbContext(opti
     public DbSet<Order> Orders { get; set; }
     public DbSet<Request> Requests { get; set; }
     public DbSet<Address> Addresses { get; set; }
+    public DbSet<ChatMessage> ChatMessages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -31,6 +32,13 @@ public class TecoContext(DbContextOptions<TecoContext> options) : DbContext(opti
         modelBuilder.Entity<Order>()
             .Property(o => o.PaymentStatus)
             .HasConversion<string>();
+
+        modelBuilder.Entity<Proposal>()
+            .HasOne(p => p.ChatMessage)
+            .WithOne(cm => cm.Proposal)
+            .HasForeignKey<Proposal>(p => p.ChatMessageId)
+            .OnDelete(DeleteBehavior.SetNull);
+
 
         modelBuilder.Entity<User>()
             .HasOne(u => u.Provider)
--- apps/backend/TecoApi/Migrations/20251013204826_ChatMessageMigration.Designer.cs
+++ apps/backend/TecoApi/Migrations/20251013204826_ChatMessageMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TecoApi.Data;
@@ -11,9 +12,11 @@ using TecoApi.Data;
 namespace TecoApi.Migrations
 {
     [DbContext(typeof(TecoContext))]
-    partial class TecoContextModelSnapshot : ModelSnapshot
+    [Migration("20251013204826_ChatMessageMigration")]
+    partial class ChatMessageMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
