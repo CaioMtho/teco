25724c389314007c0f695e57f8b518b03295bc01
CaioMtho <caiomathol@gmail.com>
2025-10-16 22:59:02 -0300

    refactor: remoção de fluxo de autenticação da api

---
+++ apps/backend/TecoApi/Migrations/20251017015234_AddSupabaseId.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TecoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSupabaseId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SupabaseId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_SupabaseUserId",
+                table: "Users",
+                column: "SupabaseId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_SupabaseUserId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SupabaseId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- apps/backend/TecoApi/Services/Interfaces/IAuthService.cs
+++
@@ -1,8 +0,0 @@
-using TecoApi.DTOs.Auth;
-
-namespace TecoApi.Services.Interfaces;
-
-public interface IAuthService
-{
-    Task<AuthResponseDto> AuthenticateAsync(LoginRequestDto loginRequest);
-}
