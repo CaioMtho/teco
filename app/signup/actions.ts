93cf9210fb52d56eaad54a42119f2dfadd5d3b80
CaioMtho <caiomathol@gmail.com>
2025-11-03 22:14:16 -0300

    fix: corrigindo mais caminhos

--- app/signup/actions.ts
+++ app/signup/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { createServerSupabaseClient } from "../../utils/supabase/server";
+import { createServerSupabaseClient } from "../../lib/supabase/server";
 
 type ActionState = {
     error?: string;
